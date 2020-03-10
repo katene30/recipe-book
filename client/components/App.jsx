f087fd9c07525c760c77670ae974c70ad690620e
Katene Jack Cotton <katenejack@gmail.com>
2020-03-11 09:33:38 +1300

    help

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+  <div>
+    <h1>Calculator!!!</h1>
+    <table>
+      <tr>
+        <th>+</th>
+        <th>-</th>
+        <th>x</th>
+        <th>/</th>
+        <th>.</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <th colspan='2' rowspan='4'>=</th>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+      </tr>
+      <tr>
+        <td colspan='3'>0</td>
+      </tr>
+    </table>
+  </div>
   )
 }
 
